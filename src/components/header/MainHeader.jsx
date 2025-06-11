2fe41504040b9c94d0b98aa62e06c599da7d2d8f
Carlos de Leon <admin@koxland.net>
2025-06-10 19:51:22 -0600

    x

--- src/components/header/MainHeader.jsx
+++ src/components/header/MainHeader.jsx
@@ -1,9 +1,16 @@
 import planetsData from "/data";
 
 function MainHeader({ onClick }) {
-  const styles = () =>
-    "text-xs font-[Inter] uppercase leading-6 font-bold cursor-pointer";
-
+  const hoverColors = {
+    mercury: "hover:text-[var(--color-mercury)]",
+    venus: "hover:text-[var(--color-venus)]",
+    earth: "hover:text-[var(--color-earth)]",
+    mars: "hover:text-[var(--color-mars)]",
+    jupiter: "hover:text-[var(--color-jupiter)]",
+    saturn: "hover:text-[var(--color-saturn)]",
+    uranus: "hover:text-[var(--color-uranus)]",
+    neptune: "hover:text-[var(--color-neptune)]",
+  };
   return (
     <div className="flex items-center justify-between border-b-1 border-[#979797] px-8 py-[22px]">
       <p className="font-[Antonio] text-[28px] font-medium uppercase">
@@ -14,7 +21,8 @@ function MainHeader({ onClick }) {
           <button
             key={planet.name.toLowerCase()}
             onClick={() => onClick(planet.name.toLowerCase())}
-            className={`${styles()} origin-center transition-transform duration-100 ease-in-out hover:scale-150 hover:text-[var(--color-${planet.name.toLowerCase()})]`}
+            className={`origin-center cursor-pointer font-[Inter] text-xs leading-6 font-bold uppercase transition-transform duration-100 ease-in-out 
+              hover:scale-150 ${hoverColors[planet.name.toLowerCase()]}`}
           >
             {planet.name}
           </button>
